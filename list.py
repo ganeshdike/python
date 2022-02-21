d54fc268e34a543452b08493633fd9c00e569699
ganeshdike <ganeshdike4636@gmail.com>
2022-02-21 13:53:58 +0530

    list program uploaded

---
+++ list.py
@@ -0,0 +1,14 @@
+name=["ganesh","nitin","pravin","rakesh","nikhil"]
+print(name[4])
+name.append("saurav");
+print(name)
+name.remove("rakesh")
+print(name)
+name2=["ganu","dike"]
+print(name+name2)
+
+a=(1,2,96,4,5,33,88)
+print(a[1:5])#slicing
+
+print(a[: :2])#Extend
+print(len(a))#length
