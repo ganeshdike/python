83eb5dc755e7cb615637b8a4aac065401d67609a
ganeshdike <ganeshdike4636@gmail.com>
2022-02-21 13:58:41 +0530

    var file uplode

---
+++ var.py
@@ -0,0 +1,31 @@
+print("Hello Wlorld...");
+no=10;
+print(no);
+
+str="ganesh";
+str1="dike";
+print(str+str1);
+
+fno=9.0;
+print(fno);
+
+a=1+3j;
+print(a);
+
+Arr="python";
+
+print(Arr[1:4]);
+#del str1;
+print(str1);
+
+b="no" in "python";
+print(b);
+
+num=input("Enter number:");
+print(num);
+
+#from math import pi
+
+print(math.pi);
+
+
